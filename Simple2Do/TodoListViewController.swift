3131f2e3eca698dc686f1cf386e7607233374351
Yongkang Lin <Yongkang.lin2002@gmail.com>
2019-04-25 22:56:56 -1000

    1

--- Simple2Do/TodoListViewController.swift
+++ Simple2Do/TodoListViewController.swift
@@ -1,48 +1,40 @@
-//
-//  TodoListViewController.swift
-//  Todoey
-//
-//  Created by Mudasir Ahmed on 12/17/17.
-//  Copyright © 2017 Mudasir Ahmed. All rights reserved.
-//
-
 import UIKit
 
-// is a TableView Controller
+//Table view controller
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Simple To Do List", "Made by Alaysia & Yong", "AP CSP 2018-2019"]
     let ToDoCell = "TodoItemCell"
 
-    // load
+//load
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    // how many sections?
+//section selection
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    // how many rows in the table?
+//table row selection
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
 
-    // cell for item
+//item cells
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ToDoCell, for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
     
-    // selection
+//selection
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         let cell = tableView.cellForRow(at: indexPath)
     
-        // if we have a check...say none
+//checkmarks
         if cell?.accessoryType == .checkmark {
             cell?.accessoryType = .none
         }
@@ -51,26 +43,26 @@ class TodoListViewController: UITableViewController {
         }
     }
     
-    // Add New Items Tapped
+//add
     @IBAction func addTapped(_ sender: Any) {
         
         let alertController = UIAlertController.init(title: "Add New Item", message: nil, preferredStyle: .alert)
         
-        // reference to the text-field
+//references text field
         var alertTextField = UITextField()
         
         alertController.addTextField { (textField) in
-            textField.placeholder = "Placeholder text"
+            textField.placeholder = "t e x t UwU"
             
-            // text-field reference
+//references text field
             alertTextField = textField
         }
-        alertController.addAction(UIAlertAction.init(title: "Add Item", style: UIAlertActionStyle.default, handler: { (action) in
+        alertController.addAction(UIAlertAction.init(title: "Add Item", style: UIAlertAction.Style.default, handler: { (action) in
             self.itemArray.append(alertTextField.text!)
             self.tableView.reloadData()
         }))
         
-        // present the alert controller
+//presentation
         self.present(alertController, animated: true, completion: nil)
     }
 }
